4b09ab36dfbce105c61ef7e5179d8f6e0f554447
Alaxkux <abdameeniboy@gmail.com>
2025-07-23 13:11:08 +0100

    Day 2: Practiced JS functions

--- day1-js-practice.js
+++ day1-js-practice.js
@@ -2,3 +2,11 @@
 let yourName = "Al-ameen";
 console.log("Hello, " + yourName);
 
+// Day 2: Functions in JavaScript
+
+function greet(name) {
+  return "Hello, " + name + "!";
+}
+
+console.log(greet("Al-ameen"));
+console.log(greet("Kuz"));
